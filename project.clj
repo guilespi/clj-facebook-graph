45e09e5b252680531b36873c59b87953ba6951ae
Kevin Noonan <kevin@chimera>
2012-11-27 14:05:54 +0000

    Added org.clojars.freeagent to project-name, for deployment to Clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-facebook-graph "0.4.0"
+(defproject org.clojars.freeagent/clj-facebook-graph "0.4.0"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.3"]
